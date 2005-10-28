8322d5997c1337ee5c132175ce49cbe89d717a00
Christophe Rhodes <csr21@cantab.net>
2005-10-28 17:20:30 +0000

    Allow capital letters in lyrics.

--- modes.lisp
+++ modes.lisp
@@ -76,7 +76,7 @@
 (loop for c in '(#\A #\B #\C #\D #\E #\F #\G #\H #\I #\J #\K #\L #\M 
 		 #\N #\O #\P #\Q #\R #\S #\T #\U #\V #\W #\X #\Y #\Z)
       for i from 65
-      do (set-key (make-insert-fun i) 'lyrics-table `((,c :shift))))
+      do (set-key (make-insert-fun i) 'lyrics-table `((,c))))
 
 (loop for c in '(#\a #\b #\c #\d #\e #\f #\g #\h #\i #\j #\k #\l #\m 
 		 #\n #\o #\p #\q #\r #\s #\t #\u #\v #\w #\x #\y #\z)
