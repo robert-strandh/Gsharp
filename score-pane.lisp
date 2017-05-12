ab0ae3034105e3b734b3503ee23f88b03c294d74
Robert Strandh <robert.strandh@gmail.com>
2017-05-12 17:38:32 +0200

    Update package prefix to reflect recent changes in McCLIM.

--- score-pane.lisp
+++ score-pane.lisp
@@ -418,8 +418,8 @@
 		(upper-tr (make-translation-transformation (round x) y))
 		(lower (sdl::ensure-beam-segment-design :down :lower (- (round (+ x inverse-slope)) (round x))))
 		(lower-tr (make-translation-transformation (round x) (+ y thickness))))
-	    (climi::medium-draw-bezier-design* medium (transform-region upper-tr upper))
-	    (climi::medium-draw-bezier-design* medium (transform-region lower-tr lower))
+	    (mcclim-bezier:medium-draw-bezier-design* medium (transform-region upper-tr upper))
+	    (mcclim-bezier:medium-draw-bezier-design* medium (transform-region lower-tr lower))
 	    (medium-draw-rectangle* medium (round x) (1+ y) (round (+ x inverse-slope)) (+ y thickness) t)))))
 
 (defmethod medium-draw-downward-beam* 
@@ -434,8 +434,8 @@
 		(upper-tr (make-translation-transformation (round x) y))
 		(lower (sdl::ensure-beam-segment-design :up :lower (- (round (+ x inverse-slope)) (round x))))
 		(lower-tr (make-translation-transformation (round x) (+ y thickness -1))))
-	    (climi::medium-draw-bezier-design* medium (transform-region upper-tr upper))
-	    (climi::medium-draw-bezier-design* medium (transform-region lower-tr lower))
+	    (mcclim-bezier:medium-draw-bezier-design* medium (transform-region upper-tr upper))
+	    (mcclim-bezier:medium-draw-bezier-design* medium (transform-region lower-tr lower))
 	    (medium-draw-rectangle* medium (round x) y (round (+ x inverse-slope)) (1- (+ y thickness)) t)))))
 
 (defmethod medium-draw-upward-beam* 
