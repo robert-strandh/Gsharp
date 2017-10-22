74cb3b604b2c5cee598e80235ea48b06ddf41c58
Robert Strandh <robert.strandh@gmail.com>
2017-10-22 18:16:48 +0200

    Update email address.

--- elasticity.lisp
+++ elasticity.lisp
@@ -1,5 +1,5 @@
 ;;; Author: Robert Strandh
-;;; Copyright (c) 2005 by Robert Strandh (strandh@labri.fr)
+;;; Copyright (c) 2005 by Robert Strandh (robert.strandh@gmail.com)
 
 ;;; This library is free software; you can redistribute it and/or
 ;;; modify it under the terms of the GNU Library General Public
