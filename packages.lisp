6d2ae03bc15c91e4f5b224e2224a61325cb38813
Robert Strandh <robert.strandh@gmail.com>
2019-08-24 11:23:53 +0200

    Remove the OBSEQ package, now in a separate component.

--- packages.lisp
+++ packages.lisp
@@ -114,15 +114,6 @@
   (:shadow #:number)
   (:export #:number))
 
-(defpackage :obseq
-  (:use :common-lisp)
-  (:export #:obseq #:obseq-elem #:obseq-next #:obseq-prev
-	   #:obseq-first-undamaged-element
-	   #:obseq-last-undamaged-element
-	   #:cost-method #:obseq-cost-method #:seq-cost
-	   #:total-cost #:combine-cost #:seq-cost-cannot-decrease
-	   #:cost-less #:obseq-solve #:obseq-interval))
-
 (defpackage :gsharp-measure
   (:use :common-lisp :gsharp-numbering :gsharp-buffer :gsharp-utilities :obseq)
   (:shadowing-import-from :gsharp-numbering #:number)
